ccbe9c40e6e0591763b92e599377c74364e45c61
Meriam <132971614+msadio8@users.noreply.github.com>
2023-08-07 16:05:19 +1000

    fixthe bug

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,13 +1,11 @@
 const path = require("path");
 
+module.export = function (app) {
+  app.get("/note", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+  });
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
-
-module.exports = app;
+  app.get("/note", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+  });
+};
